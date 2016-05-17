b9ba56a7518c75027cf8521de3bc54802d6dc935
antru6 <a.troumpoukis@gmail.com>
2016-05-17 19:18:31 +0300

    change command line arguments for rdfdump scraper main class

--- src/main/java/eu/semagrow/scraper/rdf/RdfDumpScraper.java
+++ src/main/java/eu/semagrow/scraper/rdf/RdfDumpScraper.java
@@ -4,6 +4,7 @@ import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.Rio;
 
 import java.io.File;
+import java.util.IllegalFormatCodePointException;
 
 /**
  * Created by antonis on 14/5/2015.
@@ -12,11 +13,16 @@ public class RdfDumpScraper {
 
     public static void main(String[] args) throws Exception {
 
-        MetadataGenerator generator = null;
+        String className = RdfDumpScraper.class.getName();
+        String usage = "USAGE:" +
+                "\n\t java " + className + " [input_file.nq] [endpoint_url] [-s|p|o] [output_file.n3]" +
+                "\n\t java " + className + " [input_file.nq] [endpoint_url] [-s|p|o] [subjectBound] [objectBound] [output_file.n3]";
 
-        if (args.length != 6 || (args.length == 6 && !args[2].startsWith("-"))) {
-            String className = MetadataGenerator.class.getName();
-            throw new IllegalArgumentException("USAGE: java " + className + " [input_file.nq] [endpoint_url] [-s|p|o] [subjectBound] [objectBound] [output_file.n3]");
+        if (args.length != 6 && args.length != 4) {
+            throw new IllegalArgumentException(usage);
+        }
+        if (!args[2].startsWith("-")) {
+            throw new IllegalArgumentException(usage);
         }
 
         File infile = new File(args[0]);
@@ -35,17 +41,24 @@ public class RdfDumpScraper {
 
         String whatToGenerate = args[2];
 
-        int subjectBound = Integer.valueOf(args[3]);
-        int objectBound = Integer.valueOf(args[4]);
+        int subjectBound = 0;
+        int objectBound = 0;
+        String fileName = args[3];
 
-        File outfile = new File(args[5]);
+        if (args.length == 6) {
+            subjectBound = Integer.valueOf(args[3]);
+            objectBound = Integer.valueOf(args[4]);
+            fileName = args[5];
+        }
+
+        File outfile = new File(fileName);
         if (outfile.exists() && !outfile.isFile()) {
             throw new RuntimeException("file not found: " + infile);
         }
 
         ///////////////////////////////////////////////////////////////////////
 
-        generator = new MetadataGenerator(endpoint);
+        MetadataGenerator generator = new MetadataGenerator(endpoint);
 
         generator.setFormat(format);
 
