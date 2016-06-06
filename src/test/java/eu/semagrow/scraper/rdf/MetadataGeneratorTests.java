77107eac58b3b635fb7ad46a4278719e6b724cb4
antru6 <a.troumpoukis@gmail.com>
2016-06-06 13:53:37 +0300

    new test

--- src/test/java/eu/semagrow/scraper/rdf/MetadataGeneratorTests.java
+++ src/test/java/eu/semagrow/scraper/rdf/MetadataGeneratorTests.java
@@ -1,5 +1,6 @@
 package eu.semagrow.scraper.rdf;
 
+import eu.semagrow.scraper.cassandra.CassandraScraper;
 import junit.framework.TestCase;
 
 /**
@@ -10,5 +11,10 @@ public class MetadataGeneratorTests extends TestCase {
     public void testSevod() throws Exception {
         String array[] = {"/home/antonis/datasets/SWDF.nq", "http://10.0.100.57:8899/sparql/", "-pv", "0", "0",  "bla.n3"};
         RdfDumpScraper.main(array);
+    }
+
+    public void testCassandra() throws Exception {
+        String array[] = {"127.0.0.1", "9042", "bde", "http://cassandra.semagrow.eu/", "bla.n3"};
+        CassandraScraper.main(array);
     }
 }
