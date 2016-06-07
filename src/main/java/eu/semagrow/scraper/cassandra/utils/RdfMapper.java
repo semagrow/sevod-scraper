a921bb3cc575dff23f486b827fc04dfb669a2e83
antru6 <a.troumpoukis@gmail.com>
2016-06-07 16:47:49 +0300

    minor

--- src/main/java/eu/semagrow/scraper/cassandra/utils/RdfMapper.java
+++ src/main/java/eu/semagrow/scraper/cassandra/utils/RdfMapper.java
@@ -1,11 +1,9 @@
 package eu.semagrow.scraper.cassandra.utils;
 
 import com.datastax.driver.core.DataType;
-import com.datastax.driver.core.Row;
 import eu.semagrow.scraper.cassandra.vocab.CDT;
 import eu.semagrow.scraper.cassandra.vocab.XSD;
 import org.openrdf.model.URI;
-import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.impl.ValueFactoryImpl;
 
@@ -60,12 +58,6 @@ public class RdfMapper {
         if (dataType.equals(DataType.time())) {
             return CDT.TIME;
         }
-        if (dataType.equals(DataType.date())) {
-            return CDT.DATE;
-        }
-        if (dataType.equals(DataType.time())) {
-            return CDT.TIME;
-        }
         if (dataType.equals(DataType.timestamp())) {
             return CDT.TIMESTAMP;
         }
@@ -90,46 +82,5 @@ public class RdfMapper {
         }
         throw new RuntimeException();
     }
-
-    public static Value getLiteralFromCassandraResult(Row row, String columnname) {
-        DataType dataType = row.getColumnDefinitions().getType(columnname);
-
-        if (row.getObject(columnname) == null) {
-            return vf.createLiteral("");
-        }
-
-        if (dataType.equals(DataType.ascii()) || dataType.equals(DataType.varchar()) || dataType.equals(DataType.text())) {
-            String result = row.getString(columnname);
-            if (result.startsWith("<") && result.endsWith(">")) {
-                return vf.createURI(result.substring(1,result.length()-1));
-            }
-            else {
-                return vf.createLiteral(result);
-            }
-        }
-        if (dataType.equals(DataType.varint())) {
-            return vf.createLiteral(row.getVarint(columnname).doubleValue());
-        }
-        if (dataType.equals(DataType.decimal())) {
-            return vf.createLiteral(row.getDecimal(columnname).doubleValue());
-        }
-        if (dataType.equals(DataType.cint())) {
-            return vf.createLiteral(row.getInt(columnname));
-        }
-        if (dataType.equals(DataType.bigint()) || dataType.equals(DataType.counter()) ) {
-            return vf.createLiteral(row.getLong(columnname));
-        }
-        if (dataType.equals(DataType.cdouble())) {
-            return vf.createLiteral(row.getDouble(columnname));
-        }
-        if (dataType.equals(DataType.cfloat())) {
-            return vf.createLiteral(row.getFloat(columnname));
-        }
-        if (dataType.equals(DataType.cboolean())) {
-            return vf.createLiteral(row.getBool(columnname));
-        }
-        throw new RuntimeException();  /*blob, inet, timestamp, uuid, timeuuid, list, map, set, isNULL */
-    }
-
 
 }
