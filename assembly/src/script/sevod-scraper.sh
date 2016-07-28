960a4b80e2cef2e1267f1bd86b9f820e77324e1e
antru6 <a.troumpoukis@gmail.com>
2016-07-28 17:40:53 +0300

    minor

--- assembly/src/script/sevod-scraper.sh
+++ assembly/src/script/sevod-scraper.sh
@@ -5,12 +5,12 @@ SCRIPTPATH=`pwd`
 popd > /dev/null
 
 LIB="$SCRIPTPATH/../lib"
-JARS=`ls $LIB | grep *.jar | tr '\n' ':'`
+JARS=`find $LIB -name *.jar | tr '\n' ':'`
 JAVA=java
 
 case $1 in
-   cassandra) MAIN=org.semagrow.sevod.scraper.cql.CassandraScraper
-   rdfdump) MAIN=org.semagrow.sevod.scraper.rdf.dump.RdfDumpScraper
+   cassandra) MAIN=org.semagrow.sevod.scraper.cql.CassandraScraper     ;;
+   rdfdump)   MAIN=org.semagrow.sevod.scraper.rdf.dump.RdfDumpScraper  ;;
 esac
 
 shift
