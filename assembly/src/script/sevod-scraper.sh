96f997abf18fbafa4a85e0e951af9e1341c41c94
Angelos Charalambidis <agcharal@gmail.com>
2018-11-15 12:58:47 +0200

    Remove hardcoded script name

--- assembly/src/script/sevod-scraper.sh
+++ assembly/src/script/sevod-scraper.sh
@@ -14,9 +14,9 @@ esac
 if [ -e $MAIN ]
 then
     echo "Usage:"
-    echo "./sevod-scraper.sh rdfdump [dump_file] [endpoint_url] [-s|p|o|j] [output_file]"
-    echo "./sevod-scraper.sh rdfdump [dump_file] [endpoint_url] [-s|p|o|j] [subjectTrieParameter] [objectTrieParameter] [output_file]"
-    echo "./sevod-scraper.sh cassandra [cassandra-ip-address] [cassandra-port] [cassandra-keyspace] [base] [output_file]"
+    echo "$0 rdfdump [dump_file] [endpoint_url] [-s|p|o|j] [output_file]"
+    echo "$0 rdfdump [dump_file] [endpoint_url] [-s|p|o|j] [subjectTrieParameter] [objectTrieParameter] [output_file]"
+    echo "$0 cassandra [cassandra-ip-address] [cassandra-port] [cassandra-keyspace] [base] [output_file]"
     exit
 fi
 
